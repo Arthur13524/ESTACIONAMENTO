034db82a870492393e9cca4002cb8553545903d0
Arthur13524 <arthurfeliciano271@gmail.com>
2023-03-29 01:39:02 +0900

    Project's Initial Commit

---
+++ www/components/motor.js
@@ -0,0 +1,35 @@
+function calcularconsumo() {
+    var subcompacto,  compacto, hatch, sedan,  suv, pick;
+
+    placa = document.getElementById('placa').value
+    hora = document.getElementById('hora').value
+    categoria = document.getElementById('categoria').value
+
+ if(categoria == "subcompacto") {
+     document.getElementById('nome').textContent = "R$" + hora * 3.50
+ }
+ else if(categoria == "compacto") {
+     document.getElementById('nome').textContent = "R$" + hora * 4
+ }
+ else if(categoria == "hatch") {
+     document.getElementById('nome').textContent = "R$" + hora * 4.50
+ }
+ else if(categoria == "sedan") {
+     document.getElementById('nome').textContent = "R$" + hora * 5
+ }
+ else if(categoria == "suv") {
+     document.getElementById('nome').textContent = "R$" + hora * 6
+ }
+ else if(categoria == "pick") {
+     document.getElementById('nome').textContent = "R$" + hora * 7
+ }
+}
+
+function novacompra() {
+    document.getElementById('placa').value="";
+    document.getElementById('categoria').value="cat";
+    document.getElementById('hora').value="";
+    document.getElementById('nome').innerHTML="";
+
+    alert("Os dados foram limpos");
+}
